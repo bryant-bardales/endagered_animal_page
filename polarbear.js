b797cc62df00ae6ba32046ad2158ca2a4de8bdda
Bryant Bardales <bryant.bardales@gmail.com>
2022-01-06 03:24:30 -0500

    Added space between hyperlinks on polarbear.html, added comments overall on all polarbear files, changed the title on the page and increased the font size. Also corrected an error on the index.html page that was not allowing the image to display.

--- polarbear.js
+++ polarbear.js
@@ -1,10 +1,7 @@
 
-
+//Function that changes button text from "CLICK HERE TO DONATE" to "THANK YOU FOR YOUR DONATION" for 5 seconds then changes back to the original
 function donate(button, text, textToChangebackTo){
     let donateBtn = document.getElementById(button)
     donateBtn.textContent = text;
     setTimeout(function() { document.getElementById(button).textContent = textToChangebackTo;}, 5000);
-   
-
-    
 }
