6d11442b04cc5e693ccdb5ac30f05a941f9c38cd
jviali <74158221+jviali@users.noreply.github.com>
2022-01-06 03:12:35 -0500

    -F Problem with adding image and attaching my js and css files and now website won't show.

---
+++ indexfile.js
@@ -0,0 +1,13 @@
+
+
+
+
+function donate(button, text, textToChangebackTo){
+
+    let donateBtn = document.getElementById(button)
+
+    donateBtn.textContent = text;
+
+    setTimeout(function() { document.getElementById(button).textContent = textToChangebackTo;}, 5000);
+
+}
